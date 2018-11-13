a2eaf813af00249a1760b4ac2b5744a4dfbcbb5f
GustavoER <gustavo.silva@ernetwork.com.br>
2018-11-13 18:01:04 -0200

    aula 19

---
+++ src/Arrays2.java
@@ -0,0 +1,13 @@
+public class Arrays2 {
+    public static void main(String[] args) {
+        // valores default ao criar array e não declarar valor
+        //byte, short, int, long, float, double = 0
+        // char '\u0000'''
+        // reference null
+        String[] nomes = new String[3];
+        nomes[0] = "Naruto";
+        nomes[1] = "Goku";
+        nomes[2] = "Luffy";
+
+    }
+}
