9ef2634e5c303c9b7764557db585634b4e3ce4fa
GustavoER <gustavo.silva@ernetwork.com.br>
2018-11-15 13:34:22 -0200

    aula 36

--- src/br/com/abc/core/introducaoclasses/metodos/classes/Estudante.java
+++ src/br/com/abc/core/introducaoclasses/metodos/classes/Estudante.java
@@ -1,9 +1,33 @@
 package br.com.abc.core.introducaoclasses.metodos.classes;
 
 public class Estudante {
-    public String nome;
-    public int idade;
-    public double[] notas;
+    private String nome;
+    private int idade;
+    private double[] notas;
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+    public void setNotas(double[] notas){
+        this.notas = notas;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public int getIdade(){
+        return this.idade;
+    }
+
+    public double[] getNotas(){
+        return this.notas;
+    }
 
     public void imprimirDados(){
         System.out.println("Nome: " +this.nome);
--- src/br/com/abc/core/introducaoclasses/metodos/teste/EstudanteTeste.java
+++ src/br/com/abc/core/introducaoclasses/metodos/teste/EstudanteTeste.java
@@ -5,9 +5,9 @@ import br.com.abc.core.introducaoclasses.metodos.classes.Estudante;
 public class EstudanteTeste {
     public static void main(String[] args) {
         Estudante estudante = new Estudante();
-        estudante.nome = "Gustavo";
-        estudante.idade = 26;
-        estudante.notas = new double[]{6.0, 6.0, 5.0};
+        estudante.setNome("Gustavo");
+        estudante.setIdade(26);
+        estudante.setNotas(new double[]{6.0, 6.0, 5.0});
         estudante.imprimirDados();
     }
 }
